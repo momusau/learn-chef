3a6f49ff681cb75f62bf572bf84cd9007cdaa948
mmusau <mo.musau@vion.com>
2017-10-30 18:51:45 -0400

    Added new files for ubuntu server

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "mmusau"
-client_key               "#{current_dir}/mmusau.pem"
-chef_server_url          "https://api.chef.io/organizations/oniwaban"
+node_name                "chefadmin"
+client_key               "#{current_dir}/chefadmin.pem"
+chef_server_url          "https://learnchefautomatemo.eastus.cloudapp.azure.com"
 cookbook_path            ["#{current_dir}/../cookbooks"]
