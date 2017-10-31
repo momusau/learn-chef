7dfec1b421d658a174a934fe3bd12b8392ce2e01
mmusau <mo.musau@vion.com>
2017-10-30 21:30:11 -0400

    Changed settings in knife config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,5 +5,5 @@ log_level                :info
 log_location             STDOUT
 node_name                "chefadmin"
 client_key               "#{current_dir}/chefadmin.pem"
-chef_server_url          "https://learnchefautomatemo.eastus.cloudapp.azure.com"
+chef_server_url          "https://learnchefautomatemo.eastus.cloudapp.azure.com/organizations/4thcoffee"
 cookbook_path            ["#{current_dir}/../cookbooks"]
