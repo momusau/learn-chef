db66757d3f819ec87772516a0c355eb3262d9b40
mmusau <mo.musau@vion.com>
2017-11-01 12:01:22 -0400

    Added new knife

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "chefadmin"
-client_key               "#{current_dir}/chefadmin.pem"
-chef_server_url          "https://learnchefautomatemo.eastus.cloudapp.azure.com/organizations/4thcoffee"
+node_name                "mmusau"
+client_key               "#{current_dir}/mmusau.pem"
+chef_server_url          "https://api.chef.io/organizations/oniwaban"
 cookbook_path            ["#{current_dir}/../cookbooks"]
