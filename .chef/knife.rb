044f312b28d5151c52fff5fd3d6254a56962173e
mmusau <mo.musau@vion.com>
2017-11-01 14:53:16 -0400

    testing

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,8 @@
-# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "mmusau"
-client_key               "#{current_dir}/mmusau.pem"
-chef_server_url          "https://api.chef.io/organizations/oniwaban"
+node_name                "mmusau24"
+client_key               "#{current_dir}/mmusau24.pem"
+chef_server_url          "https://api.chef.io/organizations/mibu"
 cookbook_path            ["#{current_dir}/../cookbooks"]
