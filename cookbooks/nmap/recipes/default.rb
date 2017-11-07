f1cf8e119352df35113ffadc3257bbf013d090fa
mmusau <mo.musau@vion.com>
2017-11-07 10:49:21 -0500

    test

---
+++ cookbooks/nmap/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: nmap
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+
+package 'nmap'
